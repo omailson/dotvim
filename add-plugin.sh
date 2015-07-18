627b29c8415674fc912e853c6feeb88467bbec04
Mailson Lira <mailson@gmail.com>
2015-07-18 15:51:40 -0300

    Add script to add new plugins

---
+++ add-plugin.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Add a new plugin
+# Usage: ./add-plugin.sh plugin_name repository_url
+
+if "$#" -ne 2
+then
+	echo "Invalid number of parameters"
+	exit 1
+fi
+
+git submodule add "$2" "plugins/$1"
+ln -s ../../plugins/"$1"/ vim/bundle/"$1"
+make helptags
